efdf60ee4277435b5fe3d193446031e5a866c6fa
Ivan Uzun <ivan@motorway.co.uk>
2024-08-19 23:47:26 +0100

    chore: client store, deps

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,17 +1,18 @@
 import { useUnit } from 'effector-react';
 
-import { $counter, $data } from './model';
+import { model } from './model';
 import { Queue } from './Queue';
 
 export const Page = ({ queue }: { queue: number[] }) => {
-  const [data, counter] = useUnit([$data, $counter]);
+  const [data, random, counter, inc] = useUnit([model.$data, model.$random, model.$counter, model.inc]);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       {data}
       <Queue init={queue} />
-      <a href={`/example/${counter}`}>Example: {counter}</a>
+      <a href={`/example/${random}`}>Example: {random}</a>
       <a href="mdx">Example MDX page</a>
+      <button onClick={inc}>Counter: {counter}</button>
     </div>
   );
 };
--- src/pages/index/model.ts
+++ src/pages/index/model.ts
@@ -1,13 +1,24 @@
-import { createStore, sample } from 'effector';
+import { createEvent, createStore, sample } from 'effector';
 
 import { pageStarted } from './+pageStarted';
 
+export const inc = createEvent();
+
 export const $data = createStore('', { sid: '$data' });
 
-export const $counter = createStore(0, { sid: '$counter' });
+export const $random = createStore(0, { sid: '$random' });
+
+export const $counter = createStore(0, { sid: '$counter' }).on(inc, (val) => val + 1);
 
 sample({
   clock: pageStarted,
   fn: () => Math.round(Math.random() * 1000),
-  target: $counter,
+  target: $random,
 });
+
+export const model = {
+  inc,
+  $data,
+  $random,
+  $counter,
+};
